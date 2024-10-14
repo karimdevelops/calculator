100dc6677e8b0036e613ac7164d06d5766f5f912
Abdul Karim <karim.develops@gmail.com>
2024-10-14 11:52:09 +0500

    Create a func to take operation args, returns results

--- script.js
+++ script.js
@@ -17,3 +17,23 @@ function multiply(x, y) {
 function divide(x, y) {
   return x / y;
 }
+
+function operator(firstOperand, secondOperand, arithOperator) {
+  switch (arithOperator) {
+    case "+":
+      return add(firstOperand, secondOperand);
+      break;
+    case "-":
+      return subtract(firstOperand, secondOperand);
+      break;
+    case "*":
+      return multiply(firstOperand, secondOperand);
+      break;
+    case "/":
+      return divide(firstOperand, secondOperand);
+      break;
+
+    default:
+      break;
+  }
+}
